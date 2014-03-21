6ee5f543b2b25d99627ed0ea3a6b052bf1a14a5b
Alex <alex@debian>
2014-03-21 22:30:32 +0000

    Added Twitter and Google login

--- planner/models.py
+++ planner/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from courses.models import Course
 from scheduler.models import SavedSelection
-# Create your models here.
+from django.db.models.signals import post_save
 
 class PlanCourse(models.Model):
     course = models.OneToOneField(Course)
@@ -16,3 +16,9 @@ class PlanUser(models.Model):
     	return self.user.username
     # TODO: 
     # TODO: unmet requirements
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        PlanUser.objects.create(user=instance)
+
+post_save.connect(create_user_profile, sender=User)
