f6b622618082ad9f9ea9b15c11a53665a0bbf348
Jeff <jeff@ubuntu.ubuntu-domain>
2014-03-26 14:47:12 -0400

    courses now have accompanying term

--- planner/views.py
+++ planner/views.py
@@ -14,8 +14,11 @@ def addCoursesTaken(request):
         form = CappReportField(request.POST, request.FILES)
         if form.is_valid():
             capp_report_html = request.FILES['cappReportField'].read()
-            courses_taken= RPICappReport.getCoursesTaken(capp_report_html)
-            return HttpResponse(', '.join(courses_taken))
+            courses_taken = RPICappReport.getCoursesTaken(capp_report_html)
+            print_string = []
+            for x in courses_taken:
+                print_string.append(x.name + " : " + x.term + ", ")  
+            return HttpResponse(print_string)
     else:
         form = CappReportField() 
     return render_to_response('planner/upload_capp.html', {'form': form}, context_instance=RequestContext(request))
