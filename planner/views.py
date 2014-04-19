b9f234df962c5b37724b90a873b341764d60e432
Jeff <jeff@ubuntu.ubuntu-domain>
2014-04-19 18:35:22 -0400

    each addition of a course from the capp report gets its own thread

--- planner/views.py
+++ planner/views.py
@@ -15,6 +15,9 @@ from decimal import *
 from RPICappReport import *
 from RPIRateMyProfessors import *
 
+from threading import Thread
+from time import sleep
+
 def getDifficulty(season, course_number, dept_code):
     try:
         sections = Section.objects.filter(semester__name__startswith=season,  course__number=course_number, course__department__code=dept_code)
@@ -24,6 +27,26 @@ def getDifficulty(season, course_number, dept_code):
         return Decimal(difficulty_str)
     except:
         return None
+
+def addCourseTaken(request, x):
+    if x.term == "Not Met" or len(x.term) != 6:
+        return
+    print "test"
+    x.department_prefix = x.name.split(" ")[0]
+    x.course_number = x.name.split(" ")[1]
+    x.year = x.term[:-2]
+    x.semester = map_month_to_semester(int(x.term[-2:]))
+    fall_difficulty = getDifficulty("Fall", x.course_number, x.department_prefix)
+    spring_difficulty = getDifficulty("Spring", x.course_number, x.department_prefix)
+    department = Department.objects.get(code=x.department_prefix)
+    new_plan_course = PlanCourse(
+        year=x.year, semester=x.semester,
+        user=request.user.planuser, department=department,
+        number=x.course_number, movable=False,
+        fall_difficulty=fall_difficulty, spring_difficulty=spring_difficulty
+    )
+    new_plan_course.save()
+
 
 def addCoursesTaken(request):
     if not request.user.is_authenticated():
@@ -45,23 +68,16 @@ def addCoursesTaken(request):
             # Delete all previous records, if any exist
             PlanCourse.objects.filter(user=plan_user).delete()
 
+            threads = []
+
             for x in courses_taken:
-                if x.term == "Not Met" or len(x.term) != 6:
-                    continue
-                x.department_prefix = x.name.split(" ")[0]
-                x.course_number = x.name.split(" ")[1]
-                x.year = x.term[:-2]
-                x.semester = map_month_to_semester(int(x.term[-2:]))
-                fall_difficulty = getDifficulty("Fall", x.course_number, x.department_prefix)
-                spring_difficulty = getDifficulty("Spring", x.course_number, x.department_prefix)
-                department = Department.objects.get(code=x.department_prefix)
-                new_plan_course = PlanCourse(
-                    year=x.year, semester=x.semester,
-                    user=request.user.planuser, department=department,
-                    number=x.course_number, movable=False
-                    fall_difficulty=fall_difficulty, spring_difficulty=spring_difficulty
-                )
-                new_plan_course.save()
+                thread = Thread(target = addCourseTaken, args = (request, x, ))
+                thread.start()
+                threads.append(thread)
+
+            for thread in threads:
+                thread.join()
+
             return HttpResponseRedirect("/#/planner/")
     else:
         # if request.user.planuser.first_semester:
